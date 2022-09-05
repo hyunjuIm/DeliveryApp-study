4a8f5c9a1695e6251e7811187a6cd1d9089e5c3f
hyunju <dear.jjwim@gmail.com>
2022-09-06 03:38:53 +0900

    ui 수정 : 구분선 색상 변경

--- app/src/main/java/com/hyunju/deliveryapp/widget/adapter/viewholder/order/OrderViewHolder.kt
+++ app/src/main/java/com/hyunju/deliveryapp/widget/adapter/viewholder/order/OrderViewHolder.kt
@@ -17,29 +17,28 @@ class OrderViewHolder(
 
     override fun reset() = Unit
 
-    override fun bindData(model: OrderModel) {
+    override fun bindData(model: OrderModel) = with(binding) {
         super.bindData(model)
-        with(binding) {
-            orderTitleText.text =
-                resourcesProvider.getString(R.string.order_history_title, model.orderId)
 
-            val foodMenuList = model.foodMenuList
+        orderTitleText.text = resourcesProvider.getString(
+            R.string.order_history_title, model.restaurantTitle
+        )
 
-            foodMenuList
-                .groupBy { it.title }
-                .entries.forEach { (title, menuList) ->
-                    val orderDataStr =
-                        orderContentText.text.toString() + "메뉴 : $title | 가격 : ${menuList.first().price}원 * ${menuList.size}\n"
-                    orderContentText.text = orderDataStr
-                }
-            orderContentText.text = orderContentText.text.trim()
+        val foodMenuList = model.foodMenuList
+        foodMenuList
+            .groupBy { it.title }
+            .entries.forEach { (title, menuList) ->
+                val orderDataStr =
+                    orderContentText.text.toString() +
+                            "메뉴 : $title | 가격 : ${menuList.first().price}원 * ${menuList.size}\n"
+                orderContentText.text = orderDataStr
+            }
+        orderContentText.text = orderContentText.text.trim()
 
-            orderTotalPriceText.text =
-                resourcesProvider.getString(
-                    R.string.price,
-                    foodMenuList.map { it.price }.reduce { total, price -> total + price }
-                )
-        }
+        orderTotalPriceText.text = resourcesProvider.getString(
+            R.string.price,
+            foodMenuList.map { it.price }.reduce { total, price -> total + price }
+        )
     }
 
     override fun bindViews(model: OrderModel, adapterListener: AdapterListener) {
