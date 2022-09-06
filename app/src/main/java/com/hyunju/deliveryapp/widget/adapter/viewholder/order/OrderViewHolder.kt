f8d36ef53aa804b05e989a23430887f8713a5176
hyunju <dear.jjwim@gmail.com>
2022-09-07 01:16:36 +0900

    Int 확장 함수 - 가격 콤마 표시 적용

--- app/src/main/java/com/hyunju/deliveryapp/widget/adapter/viewholder/order/OrderViewHolder.kt
+++ app/src/main/java/com/hyunju/deliveryapp/widget/adapter/viewholder/order/OrderViewHolder.kt
@@ -2,6 +2,7 @@ package com.hyunju.deliveryapp.widget.adapter.viewholder.order
 
 import com.hyunju.deliveryapp.R
 import com.hyunju.deliveryapp.databinding.ViewholderOrderBinding
+import com.hyunju.deliveryapp.extensions.fromNumberToPrice
 import com.hyunju.deliveryapp.model.restaurant.order.OrderModel
 import com.hyunju.deliveryapp.screen.base.BaseViewModel
 import com.hyunju.deliveryapp.util.provider.ResourcesProvider
@@ -30,7 +31,7 @@ class OrderViewHolder(
             .entries.forEach { (title, menuList) ->
                 val orderDataStr =
                     orderContentText.text.toString() +
-                            "메뉴 : $title | 가격 : ${menuList.first().price}원 * ${menuList.size}\n"
+                            "메뉴 : $title | 가격 : ${menuList.first().price.fromNumberToPrice()}원 * ${menuList.size}\n"
                 orderContentText.text = orderDataStr
             }
         orderContentText.text = orderContentText.text.trim()
@@ -38,6 +39,7 @@ class OrderViewHolder(
         orderTotalPriceText.text = resourcesProvider.getString(
             R.string.price,
             foodMenuList.map { it.price }.reduce { total, price -> total + price }
+                .fromNumberToPrice()
         )
     }
 
